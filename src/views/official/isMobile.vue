93745492f7d50f0d32f61f262d71fee19d0abc7c
hezhouz <46963773+hezhouz@users.noreply.github.com>
2022-10-26 17:46:11 +0800

    feat: 移动端展示

--- src/views/official/isMobile.vue
+++ src/views/official/isMobile.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>
-    手机进来的
+  <div class="ismobile">
+    <h3>问卷机器人，轻松完成问卷调研</h3>
+    <p>答题发放奖励，答题分配角色，提升问卷回收效果</p>
+    <div class="imgbox">
+      img
+    </div>
+    <p>移动端暂不支持问卷管理</p>
+    <p>请从PC端访问</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "IsMoblie"
+  name: "IsMobile"
 }
 </script>
 
-<style>
-
+<style scoped>
+.ismobile {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 </style>
